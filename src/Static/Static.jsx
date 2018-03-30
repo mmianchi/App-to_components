e870f03f1908d5eb08735a42c5c0fbb90faef7df
Mahshid M <mahshid@Mahshids-MacBook-Pro.local>
2018-03-30 00:03:28 -0700

    components

--- src/Static/Static.jsx
+++ src/Static/Static.jsx
@@ -2,43 +2,96 @@ import React from 'react';
 const Static = () => {
   return (
     <div>
-      <header class="main-header">
-        <h1 class="name"><a href="#">React Bootcamp</a></h1>
-        <ul class="main-nav">
-          <li><a href="#">My Courses</a></li>
-          <li><a href="#">Account</a></li>
-        </ul>
-      </header>
+      <Header />
 
-      <div class="banner">
-        <h1 class="headline">React Bootcamp</h1>
-        <span class="tagline">Helping you understand everything React</span>
-      </div>
+      <Banner />
 
-      <div class="row">
-        <div class="col">
-          <h2>Tutoring</h2>
-          <p>Progress starts with accountability. Be a part of a community. Find a mentor.</p>
-          <button class="btn-blue"><a href="#">Learn More</a></button>
-        </div>
+      <CardList />
 
-        <div class="col">
-          <h2>Tutorials</h2>
-          <p>Don't start from scratch, just learn the pieces that you need to advance.</p>
-          <button class="btn-blue"><a href="#">Learn More</a></button>
-        </div>
-
-        <div class="col">
-          <h2>Code Reviews</h2>
-          <p>Code isn't working out? Have a tutor review your code and get answers to your problems.</p>
-          <button class="btn-blue"><a href="#">Learn More</a></button>
-        </div>
-      </div>
-
-      <footer class="main-footer">
-        <span>&copy;2018 React Bootcamp</span>
-      </footer>
+      <Footer />
     </div>
+  );
+}
+const Header = () => {
+  return (
+    <header class="main-header">
+      <HeaderLogo /> 
+      <Navigation />
+    </header>
+  );
+}
+
+const HeaderLogo = () => {
+  return (
+    <h1 class="name">
+      <a href="#">React Bootcamp</a>
+    </h1>
+  );
+}
+
+const Navigation = () => {
+  return (
+    <ul class="main-nav">
+      <NavOne />
+      <NavTwo />
+    </ul>
+  );
+}
+
+const NavOne = () =>{
+  return (
+    <li>
+      <a href="#">My Courses</a>
+    </li>
+  );
+}
+
+const NavTwo = () => {
+  return (
+    <li>
+      <a href="#">Account</a>
+    </li>
+  );
+}
+const Banner = () => {
+  return (
+    <div class="banner">
+      <h1 class="headline">React Bootcamp</h1>
+      <span class="tagline">Helping you understand everything React</span>
+    </div>
+  );
+}
+
+const CardList = () => {
+  return (
+
+    <div class="row">
+      <div class="col">
+        <h2>Tutoring</h2>
+        <p>Progress starts with accountability. Be a part of a community. Find a mentor.</p>
+        <button class="btn-blue"><a href="#">Learn More</a></button>
+      </div>
+
+      <div class="col">
+        <h2>Tutorials</h2>
+        <p>Don't start from scratch, just learn the pieces that you need to advance.</p>
+        <button class="btn-blue"><a href="#">Learn More</a></button>
+      </div>
+
+      <div class="col">
+        <h2>Code Reviews</h2>
+        <p>Code isn't working out? Have a tutor review your code and get answers to your problems.</p>
+        <button class="btn-blue"><a href="#">Learn More</a></button>
+      </div>
+    </div>
+
+  );
+}
+const Footer = () => {
+  return (
+    <footer class="main-footer">
+      <span>&copy;2018 React Bootcamp</span>
+    </footer>
   );
 }
 
