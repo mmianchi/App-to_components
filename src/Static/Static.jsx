b38bf25852d85c38b17fe7fa8637245b4398f11c
Mahshid M <mahshid@Mahshids-MacBook-Pro.local>
2018-03-30 00:26:04 -0700

    adding components

--- src/Static/Static.jsx
+++ src/Static/Static.jsx
@@ -3,15 +3,13 @@ const Static = () => {
   return (
     <div>
       <Header />
-
       <Banner />
-
       <CardList />
-
       <Footer />
     </div>
   );
 }
+
 const Header = () => {
   return (
     <header class="main-header">
@@ -53,40 +51,70 @@ const NavTwo = () => {
     </li>
   );
 }
+
 const Banner = () => {
   return (
     <div class="banner">
-      <h1 class="headline">React Bootcamp</h1>
-      <span class="tagline">Helping you understand everything React</span>
+      <HeadLine />
+      <HeadTag />
     </div>
+  );
+}
+
+const HeadLine = () => {
+  return (
+    <h1 class="headline">React Bootcamp</h1>
+  );
+}
+
+const HeadTag = () => {
+  return (
+    <span class="tagline">Helping you understand everything React</span>
   );
 }
 
 const CardList = () => {
   return (
-
     <div class="row">
-      <div class="col">
-        <h2>Tutoring</h2>
-        <p>Progress starts with accountability. Be a part of a community. Find a mentor.</p>
-        <button class="btn-blue"><a href="#">Learn More</a></button>
-      </div>
-
-      <div class="col">
-        <h2>Tutorials</h2>
-        <p>Don't start from scratch, just learn the pieces that you need to advance.</p>
-        <button class="btn-blue"><a href="#">Learn More</a></button>
-      </div>
-
-      <div class="col">
-        <h2>Code Reviews</h2>
-        <p>Code isn't working out? Have a tutor review your code and get answers to your problems.</p>
-        <button class="btn-blue"><a href="#">Learn More</a></button>
-      </div>
+      <CardOne />
+      <CardTwo />
+      <CardThree />
     </div>
-
   );
 }
+
+const CardOne = () => {
+  return (
+    <div class="col">
+      <h2>Tutoring</h2>
+      <p>Progress starts with accountability. Be a part of a community. Find a mentor.</p>
+      <button class="btn-blue"><a href="#">Learn More</a></button>
+    </div>
+  );
+}
+
+
+const CardTwo = () => {
+  return (
+    <div class="col">
+      <h2>Tutorials</h2>
+      <p>Don't start from scratch, just learn the pieces that you need to advance.</p>
+      <button class="btn-blue"><a href="#">Learn More</a></button>
+    </div>
+  );
+}
+
+const CardThree = () => {
+  return (
+    <div class="col">
+      <h2>Code Reviews</h2>
+      <p>Code isn't working out? Have a tutor review your code and get answers to your problems.</p>
+      <button class="btn-blue"><a href="#">Learn More</a></button>
+    </div>
+  );
+}
+
+
 const Footer = () => {
   return (
     <footer class="main-footer">
