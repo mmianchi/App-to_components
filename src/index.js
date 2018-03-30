dd9268844bec243f6941a8956945e904590a42be
Mahshid M <mahshid@Mahshids-MacBook-Pro.local>
2018-03-29 23:30:44 -0700

    added css base and flexbox

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './css/base.css';
+import './css/base.css';
+import './css/flexbox.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
